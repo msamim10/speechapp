c552fcad72400ab9a3bac696d14caec70eba24d1
livewater009 <147813832+livewater009@users.noreply.github.com>
2025-05-26 17:13:50 -0700

    Bug Fixed for audio issue

---
+++ data/sounds.js
@@ -0,0 +1,8 @@
+export const SOUND_CLAPPING = require('../assets/sounds/clapping.mp3');
+export const SOUND_PRESENTATION = require('../assets/sounds/soundforpresentation.mp3');
+export const SOUND_INTREVIEW = require('../assets/sounds/soundforinterview.mp3');
+export const SOUND_SITUATIONAL_SPECIFIC = require('../assets/sounds/soundforsituationalandSpecific.mp3');
+export const SOUND_SOCIAL_CASUAL = require('../assets/sounds/soundforsocialandcasual.mp3');
+export const SOUND_VC = require('../assets/sounds/soundforvc.mp3');
+export const SOUND_SPEECH = require('../assets/sounds/soundforspeech.mp3')
+export const SOUND_PRACTICE_FUNDAMENTAL = require('../assets/sounds/soundforfundementals.mp3')
